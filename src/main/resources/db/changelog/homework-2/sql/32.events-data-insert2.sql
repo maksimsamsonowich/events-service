c1847a93e2cdb118b2461fdeede4f215f6780f49
maksimsamsonowich <maksim_samsanovich@senla.eu>
2022-04-04 13:51:50 +0300

    commit

---
+++ src/main/resources/db/changelog/homework-2/sql/32.events-data-insert2.sql
@@ -0,0 +1,16 @@
+insert into events (
+    title,
+    artists_id,
+    description,
+    occupied_places,
+    age_limit,
+    date
+)
+values (
+           'Man on the moon',
+           3,
+           'Dj Khaled a hundred racks, i made another one (uh-huh)' ||
+           'Dj Khaled with the tats, i got another one (uh-huh)',
+           15,
+           18,
+           '2021-12-16')
---
+++ src/main/resources/db/changelog/homework-2/sql/33.events-data-insert3.sql
@@ -0,0 +1,16 @@
+insert into events (
+    title,
+    artists_id,
+    description,
+    occupied_places,
+    age_limit,
+    date
+)
+values (
+           'Life of the dark rose',
+           2,
+           'Dj Khaled a hundred racks, i made another one (uh-huh)' ||
+           'Dj Khaled with the tats, i got another one (uh-huh)',
+           15,
+           18,
+           '2021-12-16')
